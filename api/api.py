0aec0c7a7f10c6fa0cb244964a014e1b16fa68ff
Jonne Okkonen <okkonen.jonne@gmail.com>
2021-02-12 21:10:55 +0200

    Create route for reading ruuvitag data
    Create config file for listing available ruuvitags

--- api/api.py
+++ api/api.py
@@ -1,9 +1,10 @@
 import flask
 from flask import request, jsonify
+import json
+from ruuvitag_sensor.ruuvitag import RuuviTag
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-
 
 @app.route('/', methods=['GET'])
 def home():
@@ -15,6 +16,37 @@ def home():
     ]
     return jsonify(response), 200
 
+@app.route('/ruuvitag/read/<name>', methods=['GET'])
+def RuuviTagRead(name):
+    ruuvitag = ""
+    # Read RuuviTags from config.json
+    with open('config.json') as f:
+        data = json.load(f)
+    # Find ruuvitag from list
+    for tag in data["ruuvitags"]:
+        if tag["name"].lower() == name.lower():
+            ruuvitag = tag
+            break
+    
+    # Check if ruuvitag was found
+    if(ruuvitag == ""):
+        return jsonify([{
+            "status": "error",
+            "message": "RuuviTag with name (" + name + ") not found."
+        }]), 404
+    else:
+        # Get Ruuvitag data
+        # Device Bluetooth Address
+        try:
+            sensor = RuuviTag(ruuvitag["address"]).update()
+        except ValueError:
+            sensor = "RuuviTag address is not correct."
+        except:
+            sensor = "Error happened while trying to read RuuviTag values."
+        return jsonify([{
+            "status": "success",
+            "message": sensor
+        }]), 200
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -25,5 +57,5 @@ def page_not_found(e):
         }
     ]
     return jsonify(response), 404
-
-app.run()
+    
+app.run(host='192.168.1.107')
